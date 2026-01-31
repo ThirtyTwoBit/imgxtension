dcba71493cdad07608aad5e6bac0e8ae354967e4
32-Bit <103079138+ThirtyTwoBit@users.noreply.github.com>
2026-01-31 17:43:01 -0600

    Update settings-btn.js

--- scripts/settings-btn.js
+++ scripts/settings-btn.js
@@ -1,21 +1,40 @@
 if (typeof window !== "undefined" && window.location.href.includes("imgflip.com/settings")) {
   const page = document.querySelector("#user-page");
 
-  const btn = document.createElement("button");
-  btn.className = "imgx-btn";
-  btn.textContent = "imgx Settings";
-  btn.style.cursor = "pointer";
-  btn.style.border = "1px solid #ccc";
-  btn.style.padding = "4px 8px";
-  btn.style.marginBottom = "10px";
+  const settingBtn = document.createElement("button");
+  settingBtn.className = "imgx-settingBtn";
+  settingBtn.textContent = "imgx Settings";
+  settingBtn.style.cursor = "pointer";
+  settingBtn.style.border = "1px solid #ccc";
+  settingBtn.style.padding = "4px 8px";
+  settingBtn.style.marginBottom = "10px";
 
-  page.prepend(btn);
+  page.prepend(settingBtn);
 
-  btn.addEventListener("click", () => {
+  settingBtn.addEventListener("click", () => {
     try {
         chrome.runtime.sendMessage({ action: "openOptions" });
     } catch (err) {
         browser.runtime.sendMessage({ action: "openOptions" });
     }
+  });
+
+  const updateBtn = document.createElement("button");
+  updateBtn.className = "imgx-settingBtn";
+  updateBtn.textContent = "imgx Logs";
+  updateBtn.style.cursor = "pointer";
+  updateBtn.style.border = "1px solid #ccc";
+  updateBtn.style.padding = "4px 8px";
+  updateBtn.style.marginBottom = "10px";
+
+  page.prepend(updateBtn);
+
+  updateBtn.addEventListener("click", () => {
+    try {
+        chrome.runtime.sendMessage({ action: "openUpdates" });
+    } catch (err) {
+        browser.runtime.sendMessage({ action: "openUpdates" });
+    }
+ 
   });
 }
